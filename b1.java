e59871928a17051dc24a743c7d3dc56c760fda5b
gayathrikotaprolu <kotaprolugayathri548@gmail.com>
2024-10-04 15:34:02 +0530

    my first project

---
+++ b1.java
@@ -0,0 +1,37 @@
+class A
+{
+ int x;
+ int y;
+ A()
+ {
+  x=10;
+  y=20;
+ }
+ void display
+{
+ system.out.println(x+" "y)
+}
+}
+class B
+{
+ int z;
+ B(inta,intb,intc)
+{
+ x=a;
+ y=b;
+ z=c;
+}
+void print()
+{
+ display ();
+ system.out.println(z);
+ }
+}
+public class Inherit
+{
+public static voidmain(string argv[])
+{
+Bb=newB(5,10,15);
+ b.print()
+}
+}
